ad866dcae1e4ed9548b3b5ac528a4d3067a29071
Loc Ha <loc.ha@nebraska.gov>
2023-04-11 14:27:36 -0500

    Minor changes

--- src/main/java/com/appslandia/common/record/AnnotationModel.java
+++ src/main/java/com/appslandia/common/record/AnnotationModel.java
@@ -21,10 +21,11 @@
 package com.appslandia.common.record;
 
 import java.lang.annotation.Annotation;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.appslandia.common.base.InitializeObject;
+import com.appslandia.common.utils.Asserts;
 import com.appslandia.common.utils.CollectionUtils;
 
 /**
@@ -32,20 +33,160 @@ import com.appslandia.common.utils.CollectionUtils;
  * @author <a href="mailto:haducloc13@gmail.com">Loc Ha</a>
  *
  */
-public class AnnotationModel {
-    final Class<? extends Annotation> annotationType;
-    final Map<String, Object> properties;
+public class AnnotationModel extends InitializeObject {
+    private Class<? extends Annotation> annotationType;
+    private Map<String, Object> properties;
 
-    public AnnotationModel(Class<? extends Annotation> annotationType, Map<String, Object> properties) {
-	this.annotationType = annotationType;
-	this.properties = CollectionUtils.hasEntries(properties) ? Collections.unmodifiableMap(new HashMap<>(properties)) : Collections.emptyMap();
+    @Override
+    protected void init() throws Exception {
+	Asserts.notNull(this.annotationType);
+
+	this.properties = CollectionUtils.unmodifiable(this.properties);
     }
 
     public Class<? extends Annotation> getAnnotationType() {
+	initialize();
 	return this.annotationType;
     }
 
+    public AnnotationModel setAnnotationType(Class<? extends Annotation> annotationType) {
+	assertNotInitialized();
+	this.annotationType = annotationType;
+	return this;
+    }
+
     public Map<String, Object> getProperties() {
+	initialize();
 	return this.properties;
+    }
+
+    protected void addProperty(String property, Object value) {
+	Asserts.notNull(property);
+	Asserts.notNull(value);
+
+	if (this.properties == null) {
+	    this.properties = new LinkedHashMap<>();
+	}
+	this.properties.put(property, value);
+    }
+
+    public AnnotationModel define(String property, String value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, Enum<?> value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, Class<?> value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, boolean value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, char value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, byte value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, short value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, int value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, long value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, float value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel define(String property, double value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, String... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, boolean... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, char... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, byte... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, short... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, int... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, long... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, float... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
+    }
+
+    public AnnotationModel defineArray(String property, double... value) {
+	assertNotInitialized();
+	addProperty(property, value);
+	return this;
     }
 }
