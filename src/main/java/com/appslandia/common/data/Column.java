bdabe371fa599d9b72deaa54e2bca0e7132543a0
Loc Ha <loc.ha@nebraska.gov>
2023-11-01 09:02:18 -0500

    Minor fixes

--- src/main/java/com/appslandia/common/data/Column.java
+++ src/main/java/com/appslandia/common/data/Column.java
@@ -259,8 +259,8 @@ public class Column extends InitializeObject implements Serializable {
     public String toString() {
 	this.initialize();
 	return STR.fmt(
-		"name={}, typeName={}, sqlType={}, columnSize={?}, fractionDigits={?}, nullable={}, position={}, columnType={}, javaType={}, tableCat={?}, tableSchema={?}, tableName={?}",
-		this.name, this.typeName, this.sqlType, this.columnSize, this.fractionDigits, this.nullable, this.position, this.columnType, this.javaType.getName(), this.tableCat,
-		this.tableSchema, this.tableName);
+		"name={}, typeName={}, sqlType={}, columnSize={?}, fractionDigits={?}, nullable={}, updatable={}, position={}, columnType={}, javaType={}, tableCat={?}, tableSchema={?}, tableName={?}",
+		this.name, this.typeName, this.sqlType, this.columnSize, this.fractionDigits, this.nullable, this.updatable, this.position, this.columnType,
+		this.javaType.getName(), this.tableCat, this.tableSchema, this.tableName);
     }
 }
