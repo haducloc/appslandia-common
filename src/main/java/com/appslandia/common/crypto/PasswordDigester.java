a8af9482d31ca736e18ad55f77b7e2dc1cc8f6dc
Loc Ha <loc.ha@nebraska.gov>
2024-11-20 15:14:26 -0600

    Minor fixes

--- src/main/java/com/appslandia/common/crypto/PasswordDigester.java
+++ src/main/java/com/appslandia/common/crypto/PasswordDigester.java
@@ -73,20 +73,20 @@ public class PasswordDigester extends TextDigester {
 
     byte[] salt = CryptoUtils.randomBytes(this.saltSize);
     char[] pwdChars = password.toCharArray();
+    PBEKeySpec keySpec = new PBEKeySpec(pwdChars, salt, this.iterationCount, this.keySize * 8);
 
     try {
-      PBEKeySpec keySpec = new PBEKeySpec(pwdChars, salt, this.iterationCount, this.keySize * 8);
       SecretKey secret = this.getImpl().generateSecret(keySpec);
-      keySpec.clearPassword();
-
       byte[] storedHash = secret.getEncoded();
       CryptoUtils.destroy(secret);
+
       return this.baseEncoder.encode(ArrayUtils.append(salt, storedHash));
 
     } catch (GeneralSecurityException ex) {
       throw new CryptoException(ex.getMessage(), ex);
     } finally {
       CryptoUtils.clear(pwdChars);
+      keySpec.clearPassword();
     }
   }
 
