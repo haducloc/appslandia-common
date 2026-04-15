f9b1819f3cb5106ab830a91e1529e312cc9ea4ef
Loc Ha <loc.ha@nebraska.gov>
2026-04-15 10:34:31 -0500

    Minor fixes

--- src/main/java/com/appslandia/common/utils/NormalizeUtils.java
+++ src/main/java/com/appslandia/common/utils/NormalizeUtils.java
@@ -20,8 +20,12 @@
 
 package com.appslandia.common.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.text.Normalizer;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -67,46 +71,58 @@ public class NormalizeUtils {
     return STRIP_ACCENTS_PATTERN.matcher(decomposed).replaceAll(StringUtils.EMPTY_STRING);
   }
 
-  private static final Pattern WTSP_PUNCT_PATTERN = Pattern.compile("[\\s\\p{Punct}]+");
+  private static final Pattern WSPT1_PATTERN = Pattern.compile("[\\s\\p{Punct}]+");
 
   public static String normalizeLabel(String str) {
-    str = normalize(str, "-", WTSP_PUNCT_PATTERN);
+    str = normalize(str, "-", WSPT1_PATTERN);
     str = StringUtils.trimChar(str, '-');
+
     return StringUtils.toLowerCase(str, Locale.ROOT);
   }
 
-  private static final Pattern SP2_PATTERN = Pattern.compile(" {2,}");
-  private static final Pattern CRLF_PATTERN = Pattern.compile("\r?\n");
-  private static final Pattern CRLF3_PATTERN = Pattern.compile("(\r?\n\\s*){3,}");
-
-  private static final String LINE_SEP1 = System.lineSeparator();
-  private static final String LINE_SEP2 = System.lineSeparator().repeat(2);
+  private static final Pattern CRLF3_PATTERN = Pattern.compile("(\r?\n){3,}");
 
   public static String normalizeText(String text) {
     if (text == null) {
       return null;
     }
-    text = normalize(text, " ", SP2_PATTERN);
-    text = normalize(text, LINE_SEP1, CRLF_PATTERN);
-    text = normalize(text, LINE_SEP2, CRLF3_PATTERN);
+    var br = new BufferedReader(new StringReader(text));
+    List<String> lines = null;
+    try {
+      lines = IOUtils.readAllLines(br);
+    } catch (IOException ex) {
+      // Never happen
+    }
 
+    for (var i = 0; i < lines.size(); i++) {
+      var line = lines.get(i).stripTrailing();
+      lines.set(i, !line.isBlank() ? line : "");
+    }
+
+    // Build Text
+    text = String.join(System.lineSeparator(), lines);
+
+    // Collapse (\r?\n){3,}
+    text = normalize(text, System.lineSeparator().repeat(2), CRLF3_PATTERN);
     return text;
   }
 
-  private static final Pattern WTSP_PATTERN = Pattern.compile("\\s+");
+  private static final Pattern WS1_PATTERN = Pattern.compile("\\s+");
 
   public static String normalizeString(String str) {
     if (str == null) {
       return null;
     }
-    return normalize(str, " ", WTSP_PATTERN);
+    return normalize(str, " ", WS1_PATTERN);
   }
+
+  private static final Pattern CRLF1_PATTERN = Pattern.compile("(\r?\n){1,}");
 
   public static String normalizeHtml(String html) {
     if (html == null) {
       return null;
     }
-    html = normalize(html, "", CRLF_PATTERN);
+    html = normalize(html, "", CRLF1_PATTERN);
     return html;
   }
 
@@ -114,14 +130,14 @@ public class NormalizeUtils {
     if (str == null) {
       return null;
     }
-    return normalize(str, "", WTSP_PATTERN);
+    return normalize(str, "", WS1_PATTERN);
   }
 
   public static String removeSpPunct(String str) {
     if (str == null) {
       return null;
     }
-    return normalize(str, "", WTSP_PUNCT_PATTERN);
+    return normalize(str, "", WSPT1_PATTERN);
   }
 
   private static final Pattern NON_DIGITS_PATTERN = Pattern.compile("[^\\d]+");
